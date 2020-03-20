df11aa1ce91d4a6a7d41a934599a50a0d539381d
Alessio Catania <catania.alessio96@gmail.com>
2020-03-20 11:37:34 +0100

    Ho aggiunto il no priv e il controllo del nonce

--- includes/Api/ApiRequest.php
+++ includes/Api/ApiRequest.php
@@ -18,11 +18,19 @@ class ApiRequest {
 	 */
 	public function run() {
 		add_action( 'wp_ajax_submitVideo', array( $this, 'send_request' ) );
+		add_action( 'wp_ajax_nopriv_submitVideo', array( $this, 'send_request' ) );
 	}
 	/**
 	 * Da qui invierò la richiesta HTTP.
 	 */
 	public function send_request() {
-		wp_send_json_success();
+		if ( isset( $_POST['_ajax_nonce'] ) ) {
+			$nonce = sanitize_text_field( wp_unslash( $_POST['_ajax_nonce'] ) );
+			if ( check_ajax_referer( 'itr_ajax_nonce', $nonce ) ) {
+				wp_send_json_success();
+			}
+		}
+		print 'Sorry, your nonce did not verify.';
+		wp_die();
 	}
 }
