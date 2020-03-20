83d6cbc99b4c1c1bd89eafcb274c8f20bba8439e
Alessio Catania <catania.alessio96@gmail.com>
2020-03-20 16:38:18 +0100

    add test case

--- tests/test-api.php
+++ tests/test-api.php
@@ -57,4 +57,22 @@ class TestApiRequest extends WP_Ajax_UnitTestCase {
        $response = json_decode( $this->_last_response );
        $this->assertFalse( $response->success);
      }
+     /**
+      * Effettua la chiamata anche se il video è già stato convertito
+      */
+     public function test_subtitle_already_exists_send_request(){
+       $_POST['_ajax_nonce'] = wp_create_nonce( 'itr_ajax_nonce' );
+       $_POST['id_attachment'] = 1;
+       $_POST['src_attachment'] = '#';
+       $_POST['id_post'] = 1;
+       add_post_meta($_POST['id_attachment'],'ear2words_subtitle_video',1);
+       add_option('ea2words_license_key','teststst');
+       try {
+           $this->_handleAjax( 'submitVideo' );
+       } catch ( WPAjaxDieContinueException $e ) {}
+       // Verifica che è stata lanciata l'eccezione
+       $this->assertTrue( isset( $e ) );
+       $response = json_decode( $this->_last_response );
+       $this->assertFalse( $response->success);
+     }
 }
