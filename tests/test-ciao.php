befb7b49deddd453b0bd02c8818dc6cba0325062
Nicola Palermo <nicola.palermo@ctmobi.it>
2020-03-20 10:36:48 +0100

    test

---
+++ tests/test-ciao.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Class Ciao
+ *
+ * @package Ear2words
+ */
+
+/**
+ * Sample test case.
+ */
+class Ciao extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_saluta() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+		// return "ciao";
+	}
+}
