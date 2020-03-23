7e4414ce108811e826650f16f785d89dfc1b69bb
Alessio Catania <catania.alessio96@gmail.com>
2020-03-23 17:31:26 +0100

    fix nomi

--- includes/Gutenberg/VideoBlock.php
+++ includes/Gutenberg/VideoBlock.php
@@ -17,15 +17,15 @@ class VideoBlock {
 	 * Init class actions
 	 */
 	public function run() {
-		add_action( 'enqueue_block_editor_assets', array( $this, 'myguten_enqueue' ) );
+		add_action( 'enqueue_block_editor_assets', array( $this, 'add_subtitle_button_enqueue' ) );
 	}
 	/**
 	 * Enqueue degli script.
 	 */
-	public function myguten_enqueue() {
-		wp_enqueue_script( 'myguten-script', plugins_url( '../../dist/bloks.build.js', __FILE__ ), array( 'wp-blocks' ), 'myguten', false );
+	public function add_subtitle_button_enqueue() {
+		wp_enqueue_script( 'add_subtitle_button-script', plugins_url( '../../dist/blocks.build.js', __FILE__ ), array( 'wp-blocks' ), 'add_subtitle_button', false );
 		wp_localize_script(
-			'myguten-script',
+			'add_subtitle_button-script',
 			'my_ajax_object',
 			array(
 				'ajax_url'  => admin_url( 'admin-ajax.php' ),
