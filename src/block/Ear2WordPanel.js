8bbab546d05f0123b2a1373ba11fc2ff8ea5227f
Alessio Catania <catania.alessio96@gmail.com>
2020-03-25 17:24:51 +0100

    revisione codice addsubtitlebutton

---
+++ src/block/Ear2WordPanel.js
@@ -0,0 +1,58 @@
+/*  global ear2words_button_object, XMLHttpRequest  */
+import { useSelect } from "@wordpress/data";
+
+const Ear2WordPanel = props => {
+	const idPost = useSelect(select =>
+		select("core/editor").getCurrentPostId()
+	);
+	const { PanelBody, Button } = wp.components;
+	const { InspectorControls } = wp.editor;
+	function onClick() {
+		props.setAttributes({ hasRequest: true });
+		const idAttachment = props.attributes.id;
+		const srcAttachment = props.attributes.src;
+		const xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function ajax() {
+			if (this.readyState === 4 && this.status === 200) {
+				const response = JSON.parse(this.response);
+				if (response.success) {
+					wp.data
+						.dispatch("core/notices")
+						.createNotice("success", "Job inviato correttamente");
+				} else {
+					wp.data
+						.dispatch("core/notices")
+						.createNotice(
+							"error",
+							"ERRORE, job non inviato correttamente"
+						);
+				}
+			}
+		};
+		xhttp.open("POST", ear2words_button_object.ajax_url, true);
+		xhttp.setRequestHeader(
+			"Content-type",
+			"application/x-www-form-urlencoded"
+		);
+		xhttp.send(
+			`action=submitVideo&_ajax_nonce=${ear2words_button_object.ajaxnonce}&id_attachment=${idAttachment}&src_attachment=${srcAttachment}&id_post=${idPost}`
+		);
+	}
+	return (
+		<InspectorControls>
+			<PanelBody title="Ear2words">
+				<Button
+					disabled={props.attributes.hasRequest}
+					name="sottotitoli"
+					id={props.attributes.id}
+					isPrimary
+					onClick={onClick}
+				>
+					ATTIVA SOTTOTITOLI
+				</Button>
+			</PanelBody>
+		</InspectorControls>
+	);
+};
+
+export default Ear2WordPanel;
--- src/block/addSubtitleButton.js
+++ src/block/addSubtitleButton.js
@@ -1,81 +1,32 @@
-/*  global ear2words_button_object, XMLHttpRequest  */
-import { useSelect } from "@wordpress/data";
 import { createHigherOrderComponent } from "@wordpress/compose";
+import Ear2WordPanel from "./Ear2WordPanel";
 const backgroundSettings = {
 	hasRequest: {
 		type: "boolean"
 	}
 };
 
-const withInspectorControls = createHigherOrderComponent(
-	BlockEdit =>
-		function AddElement(props) {
-			const idPost = useSelect(select =>
-				select("core/editor").getCurrentPostId()
-			);
-			const { Fragment } = wp.element;
-			const { InspectorControls } = wp.editor;
-			const { PanelBody, Button } = wp.components;
-			function onClick() {
-				props.setAttributes({ hasRequest: true });
-				const idAttachment = props.attributes.id;
-				const srcAttachment = props.attributes.src;
-				const xhttp = new XMLHttpRequest();
-				xhttp.onreadystatechange = function ajax() {
-					if (this.readyState === 4 && this.status === 200) {
-						const response = JSON.parse(this.response);
-						if (response.success) {
-							wp.data
-								.dispatch("core/notices")
-								.createNotice(
-									"success",
-									"Job inviato correttamente"
-								);
-						} else {
-							wp.data
-								.dispatch("core/notices")
-								.createNotice(
-									"error",
-									"ERRORE, job non inviato correttamente"
-								);
-						}
-					}
-				};
-				xhttp.open("POST", ear2words_button_object.ajax_url, true);
-				xhttp.setRequestHeader(
-					"Content-type",
-					"application/x-www-form-urlencoded"
-				);
-				xhttp.send(
-					`action=submitVideo&_ajax_nonce=${ear2words_button_object.ajaxnonce}&id_attachment=${idAttachment}&src_attachment=${srcAttachment}&id_post=${idPost}`
-				);
-			}
-			if (props.name !== "core/video") {
-				return (
-					<Fragment>
-						<BlockEdit {...props} />
-					</Fragment>
-				);
-			}
+const withInspectorControls = BlockEdit => {
+	return props => {
+		const { Fragment } = wp.element;
+		if (props.name !== "core/video") {
 			return (
 				<Fragment>
 					<BlockEdit {...props} />
-					<InspectorControls>
-						<PanelBody title="Ear2words">
-							<Button
-								disabled={props.attributes.hasRequest}
-								name="sottotitoli"
-								id={props.attributes.id}
-								isPrimary
-								onClick={onClick}
-							>
-								ATTIVA SOTTOTITOLI
-							</Button>
-						</PanelBody>
-					</InspectorControls>
 				</Fragment>
 			);
-		},
+		}
+		return (
+			<Fragment>
+				<BlockEdit {...props} />
+				<Ear2WordPanel {...props} />
+			</Fragment>
+		);
+	};
+};
+
+const ExtendVideoBlock = createHigherOrderComponent(
+	withInspectorControls,
 	"withInspectorControls"
 );
 
@@ -95,5 +46,5 @@ wp.hooks.addFilter(
 wp.hooks.addFilter(
 	"editor.BlockEdit",
 	"ear2words/with-inspector-controls",
-	withInspectorControls
+	ExtendVideoBlock
 );
