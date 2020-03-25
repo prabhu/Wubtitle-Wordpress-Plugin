a22ee55f7b871d17c0d9b74a437de6b6123b050e
Alessio Catania <catania.alessio96@gmail.com>
2020-03-25 19:47:52 +0100

    ho riscritto la chiamata ajax con apifetch

--- src/block/Ear2WordPanel.js
+++ src/block/Ear2WordPanel.js
@@ -1,5 +1,6 @@
-/*  global ear2words_button_object, XMLHttpRequest  */
+/*  global ear2words_button_object  */
 import { useSelect } from "@wordpress/data";
+import apiFetch from "@wordpress/api-fetch";
 
 const Ear2WordPanel = props => {
 	const idPost = useSelect(select =>
@@ -11,32 +12,28 @@ const Ear2WordPanel = props => {
 		props.setAttributes({ hasRequest: true });
 		const idAttachment = props.id;
 		const srcAttachment = props.src;
-		const xhttp = new XMLHttpRequest();
-		xhttp.onreadystatechange = function ajax() {
-			if (this.readyState === 4 && this.status === 200) {
-				const response = JSON.parse(this.response);
-				if (response.success) {
-					wp.data
-						.dispatch("core/notices")
-						.createNotice("success", "Job inviato correttamente");
-				} else {
-					wp.data
-						.dispatch("core/notices")
-						.createNotice(
-							"error",
-							"ERRORE, job non inviato correttamente"
-						);
-				}
+		apiFetch({
+			url: ear2words_button_object.ajax_url,
+			method: "POST",
+			headers: {
+				"Content-Type":
+					"application/x-www-form-urlencoded; charset=utf-8"
+			},
+			body: `action=submitVideo&_ajax_nonce=${ear2words_button_object.ajaxnonce}&id_attachment=${idAttachment}&src_attachment=${srcAttachment}&id_post=${idPost}`
+		}).then(res => {
+			if (res.success) {
+				wp.data
+					.dispatch("core/notices")
+					.createNotice("success", "Job inviato correttamente");
+			} else {
+				wp.data
+					.dispatch("core/notices")
+					.createNotice(
+						"error",
+						"ERRORE, job non inviato correttamente"
+					);
 			}
-		};
-		xhttp.open("POST", ear2words_button_object.ajax_url, true);
-		xhttp.setRequestHeader(
-			"Content-type",
-			"application/x-www-form-urlencoded"
-		);
-		xhttp.send(
-			`action=submitVideo&_ajax_nonce=${ear2words_button_object.ajaxnonce}&id_attachment=${idAttachment}&src_attachment=${srcAttachment}&id_post=${idPost}`
-		);
+		});
 	}
 	return (
 		<InspectorControls>
