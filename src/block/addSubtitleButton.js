6e1d4aa8db7db7aed51a9cfb3cfd1dba8b92fcb8
Alessio Catania <catania.alessio96@gmail.com>
2020-03-26 09:47:32 +0100

    import fragment

--- src/block/addSubtitleButton.js
+++ src/block/addSubtitleButton.js
@@ -1,10 +1,10 @@
 import { createHigherOrderComponent } from "@wordpress/compose";
 import { addFilter } from "@wordpress/hooks";
 import Ear2WordPanel from "./Ear2WordPanel";
+import { Fragment } from "@wordpress/element";
 
 const withInspectorControls = BlockEdit => {
 	return props => {
-		const { Fragment } = wp.element;
 		if (props.name !== "core/video") {
 			return <BlockEdit {...props} />;
 		}
