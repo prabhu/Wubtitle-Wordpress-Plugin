621e241aeb6842ff912515079960a2ab618224a8
Alessio Catania <catania.alessio96@gmail.com>
2020-03-30 13:27:47 +0200

    aggiornamento body richiesta

--- includes/Api/ApiRequest.php
+++ includes/Api/ApiRequest.php
@@ -31,32 +31,28 @@ class ApiRequest {
 			$nonce          = sanitize_text_field( wp_unslash( $_POST['_ajax_nonce'] ) );
 			$id_attachment  = sanitize_text_field( wp_unslash( $_POST['id_attachment'] ) );
 			$src_attachment = sanitize_text_field( wp_unslash( $_POST['src_attachment'] ) );
-			$id_post        = sanitize_text_field( wp_unslash( $_POST['id_post'] ) );
 			$subtitle       = get_post_meta( $id_attachment, 'ear2words_subtitle_video' );
-			$domain_name    = str_replace( 'http://', '', get_site_url() );
 		if ( ! empty( $subtitle ) ) {
 			wp_send_json_error( 'Errore,sottotitoli già esistenti per il video selezionato' );
 		}
 		if ( ! check_ajax_referer( 'itr_ajax_nonce', $nonce ) ) {
 			wp_send_json_error( 'Errore, richiesta non valida' );
 		}
-			$body     = array(
+			$file       = get_attached_file( $id_attachment );
+			$video_data = wp_read_video_metadata( $file );
+			$body       = array(
 				'data' => array(
-					'article' => array(
-						'id' => (int) $id_post,
-					),
-					'video'   => array(
-						'id'  => (int) $id_attachment,
-						'url' => $src_attachment,
-					),
+					'attachmentId' => (int) $id_attachment,
+					'url'          => $src_attachment,
+					'size'         => $video_data['filesize'],
+					'duration'     => $video_data['length'],
 				),
 			);
-			$response = wp_remote_post(
+			$response   = wp_remote_post(
 				ENDPOINT_URL,
 				array(
 					'method'  => 'POST',
 					'headers' => array(
-						'domainName'   => $domain_name,
 						'licenseKey'   => $license_key,
 						'Content-Type' => 'application/json; charset=utf-8',
 					),
