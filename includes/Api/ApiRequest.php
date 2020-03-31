5960a01ebfe1ad11bc35ce9605a37ae664583b73
Alessio Catania <catania.alessio96@gmail.com>
2020-03-31 17:46:06 +0200

    aggiornato test e apirequest

--- includes/Api/ApiRequest.php
+++ includes/Api/ApiRequest.php
@@ -28,34 +28,70 @@ class ApiRequest {
 	public function get_media_metadata( $id_attachment ) {
 		return wp_get_attachment_metadata( $id_attachment );
 	}
+
+	/**
+	 * Verifica la validazione
+	 *
+	 *  @param array  $array post.
+	 *  @param string $license_key licenza utente.
+	 */
+	public function is_not_valid( $array, $license_key ) {
+		if ( ! isset( $array['id_attachment'] ) || ! isset( $array['src_attachment'] ) || empty( $license_key ) ) {
+			$array['check'] = true;
+			return $array;
+		}
+		$array['check'] = false;
+		return $array;
+	}
+	/**
+	 *  Creo il body della richiesta.
+	 *
+	 * @param int    $id_attachment id del video.
+	 * @param string $src_attachment url del video.
+	 */
+	public function set_body_request( $id_attachment, $src_attachment ) {
+		$id_attachment = (int) $id_attachment;
+		$video_data    = $this->get_media_metadata( $id_attachment );
+		if ( ! is_numeric( $id_attachment ) || $video_data['filesize'] <= 0 || $video_data['length'] <= 0 || ! filter_var( $src_attachment, FILTER_VALIDATE_URL ) ) {
+			return false;
+		}
+		$body = array(
+			'data' => array(
+				'attachmentId' => (int) $id_attachment,
+				'url'          => $src_attachment,
+				'size'         => $video_data['filesize'],
+				'duration'     => $video_data['length'],
+			),
+		);
+		return $body;
+	}
 	/**
 	 * Da qui invierò la richiesta HTTP.
 	 */
 	public function send_request() {
 		$license_key = get_option( 'ear2words_license_key' );
-		if ( ! isset( $_POST['_ajax_nonce'] ) || ! isset( $_POST['id_attachment'] ) || ! isset( $_POST['src_attachment'] ) || ! isset( $_POST['id_post'] ) || empty( $license_key ) ) {
+		if ( ! isset( $_POST['_ajax_nonce'] ) ) {
 			wp_send_json_error( 'Errore, richiesta non valida' );
 		}
-			$nonce          = sanitize_text_field( wp_unslash( $_POST['_ajax_nonce'] ) );
-			$id_attachment  = sanitize_text_field( wp_unslash( $_POST['id_attachment'] ) );
-			$src_attachment = sanitize_text_field( wp_unslash( $_POST['src_attachment'] ) );
+		$nonce = sanitize_text_field( wp_unslash( $_POST['_ajax_nonce'] ) );
+		if ( ! check_ajax_referer( 'itr_ajax_nonce', $nonce ) ) {
+			wp_send_json_error( 'Errore, richiesta non valida' );
+		}
+		$data_attachment = $this->is_not_valid( $_POST, $license_key );
+		if ( $data_attachment['check'] ) {
+			wp_send_json_error( 'Errore, richiesta non valida' );
+		}
+			$id_attachment  = sanitize_text_field( wp_unslash( $data_attachment['id_attachment'] ) );
+			$src_attachment = sanitize_text_field( wp_unslash( $data_attachment['src_attachment'] ) );
 			$subtitle       = get_post_meta( $id_attachment, 'ear2words_subtitle_video' );
 		if ( ! empty( $subtitle ) ) {
 			wp_send_json_error( 'Errore,sottotitoli già esistenti per il video selezionato' );
 		}
-		if ( ! check_ajax_referer( 'itr_ajax_nonce', $nonce ) ) {
+			$body = $this->set_body_request( $id_attachment, $src_attachment );
+		if ( ! $body ) {
 			wp_send_json_error( 'Errore, richiesta non valida' );
 		}
-			$video_data = $this->get_media_metadata( $id_attachment );
-			$body       = array(
-				'data' => array(
-					'attachmentId' => (int) $id_attachment,
-					'url'          => $src_attachment,
-					'size'         => $video_data['filesize'],
-					'duration'     => $video_data['length'],
-				),
-			);
-			$response   = wp_remote_post(
+			$response = wp_remote_post(
 				ENDPOINT_URL,
 				array(
 					'method'  => 'POST',
--- tests/test-api.php
+++ tests/test-api.php
@@ -16,6 +16,7 @@ class TestApiRequest extends WP_Ajax_UnitTestCase {
    public function SetUp(){
      parent::setUp();
      update_option('siteurl','http://wordpress01.local');
+     $this->instance = new Ear2Words\Api\ApiRequest();
    }
    /**
     * tearDown function.
@@ -23,34 +24,7 @@ class TestApiRequest extends WP_Ajax_UnitTestCase {
     public function tearDown(){
       parent::tearDown();
     }
-  /**
-   * Effettua la chiamata al job correttamente
-   */
-   public function test_positive_send_request(){
-     $video_data = array(
-       'filesize' => 123456789,
-       'length'   => 155,
-     );
-     $send_request = $this->getMockBuilder(Ear2Words\Api\ApiRequest::class)->setMethods(['get_media_metadata'])->getMock();
-     $send_request->expects($this->any())->method("get_media_metadata")->willReturn($video_data);
-     $_POST['_ajax_nonce'] = wp_create_nonce( 'itr_ajax_nonce' );
-     $_POST['id_attachment'] = 1;
-     $_POST['src_attachment'] = '#';
-     $_POST['id_post'] = 1;
-     // $_GET['action'] = 'submitVideo';
-     // $_POST['action'] = 'submitVideo';
-     // $_REQUEST = array_merge( $_POST, $_GET );
 
-     add_option('ear2words_license_key','4465c448e4bb078b96c2a3719a28ff41480c53f3e367bf3a505a8628ddb49478');
-     try {
-        $this->_handleAjax( 'submitVideo ');
-     } catch ( WPAjaxDieContinueException $e ) {
-     }
-     $response = json_decode( $this->_last_response );
-     error_log(print_r($response,true));
-     $this->assertTrue( $response->success );
-     $this->assertEquals( 201, $response->data );
-   }
    /**
     * Effuettua la chiamata senza nonce
     */
@@ -81,39 +55,64 @@ class TestApiRequest extends WP_Ajax_UnitTestCase {
        $this->assertFalse( $response->success);
      }
      /**
-      * Effettua la chiamata anche se il video è già stato convertito
+      * Effettua la chiamata validando tutti i campi
       */
-     public function test_subtitle_already_exists_send_request(){
-       $_POST['_ajax_nonce'] = wp_create_nonce( 'itr_ajax_nonce' );
-       $_POST['id_attachment'] = 1;
-       $_POST['src_attachment'] = '#';
-       $_POST['id_post'] = 1;
-       add_post_meta($_POST['id_attachment'],'ear2words_subtitle_video',1);
-       add_option('ea2words_license_key','teststst');
-       try {
-           $this->_handleAjax( 'submitVideo' );
-       } catch ( WPAjaxDieContinueException $e ) {}
-       // Verifica che è stata lanciata l'eccezione
-       $this->assertTrue( isset( $e ) );
-       $response = json_decode( $this->_last_response );
-       $this->assertFalse( $response->success);
-     }
-     /**
-      * Effettua la chiamata al job con una license key non valida
-      */
-      public function test_invalid_license_send_request(){
+      public function test_validate_field(){
         $_POST['_ajax_nonce'] = wp_create_nonce( 'itr_ajax_nonce' );
-        $_POST['id_attachment'] = 1;
         $_POST['src_attachment'] = '#';
-        $_POST['id_post'] = 1;
-        add_option('ear2words_license_key','licensenonvalida');
-        try {
-            $this->_handleAjax( 'submitVideo' );
-        } catch ( WPAjaxDieContinueException $e ) {}
-        // Verifica che è stata lanciata l'eccezione
-        $this->assertTrue( isset( $e ) );
-        $response = json_decode( $this->_last_response );
-        $this->assertTrue( $response->success );
-        $this->assertEquals( 403, $response->data );
+        $_POST['id_attachment'] = 1;
+        $license_key = '000';
+        $result = $this->instance->is_not_valid($_POST,$license_key);
+        $this->assertFalse($result['check']);
       }
+      /**
+       * Verifica che il body è stato creato correttamente
+       */
+       public function test_body_request(){
+         $src = 'http://test';
+         $attachment_data = array(
+            'guid'           => '/test',
+            'post_mime_type' => 'video',
+            'post_title'     => 'test',
+            'post_content'   => '',
+            'post_status'    => 'inherit'
+          );
+          $attachment_metadata = array(
+            'filesize' => 123456,
+            'length'   => 15,
+          );
+         $attachment_id = self::factory()->attachment->create($attachment_data,'/test',1);
+         wp_update_attachment_metadata( $attachment_id, $attachment_metadata );
+         $result = $this->instance->set_body_request($attachment_id,$src);
+         $body       = array(
+    			 'data' => array(
+    				 'attachmentId' => $attachment_id,
+    				 'url'          => $src,
+    				 'size'         => 123456,
+    				 'duration'     => 15,
+    			 ),
+    		 );
+         $this->assertEqualSets($body,$result);
+       }
+       /**
+        * Effettua la chiamata con un url non valida
+        */
+        public function test_fail_body_request(){
+          $src = 'invalidurl';
+          $attachment_data = array(
+             'guid'           => '/test',
+             'post_mime_type' => 'video',
+             'post_title'     => 'test',
+             'post_content'   => '',
+             'post_status'    => 'inherit'
+           );
+           $attachment_metadata = array(
+             'filesize' => 123456,
+             'length'   => 15,
+           );
+          $attachment_id = self::factory()->attachment->create($attachment_data,'/test',1);
+          wp_update_attachment_metadata( $attachment_id, $attachment_metadata );
+          $result = $this->instance->set_body_request($attachment_id,$src);
+          $this->assertFalse($result);
+        }
 }
