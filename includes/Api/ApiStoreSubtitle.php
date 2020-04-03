16c0a2ad211fccacc35800475b97cf8d7bae0b73
Nicola Palermo <nicola.palermo@ctmobi.it>
2020-04-03 11:10:24 +0200

    modifiche fixphp

--- includes/Api/ApiStoreSubtitle.php
+++ includes/Api/ApiStoreSubtitle.php
@@ -44,8 +44,7 @@ class ApiStoreSubtitle {
 	public function auth_and_get_subtitle( $request ) {
 		$headers        = $request->get_headers();
 		$jwt            = $headers['jwt'][0];
-		$params         = $request->get_params();		
-		$file           = $request->get_file_params();		
+		$params         = $request->get_param( 'data' );
 		$db_license_key = get_option( 'ear2words_license_key' );
 		try {
 			JWT::decode( $jwt, $db_license_key, array( 'HS256' ) );
@@ -59,17 +58,45 @@ class ApiStoreSubtitle {
 			);
 			return $error;
 		}
-		return $this->get_subtitle( $params, $file );
+		return $this->get_subtitle( $params );
 	}
 
 	/**
 	 * Ottiene.
 	 *
-	 * @param string $body file.
+	 * @param array $params parametri del file.
 	 */
-	public function get_subtitle( $params, $file ) {
-		//TODO: Implementare salvataggio file
-		wp_send_json_success( ["message" => "ciao"] );
+	public function get_subtitle( $params ) {
+		$url           = $params['url'];
+		$id_attachment = $params['attachmentId'];
+
+		$temp_file = download_url( $url );
+
+		if ( is_wp_error( $temp_file ) ) {
+			wp_send_json_success( array( 'message' => 'invalid url' ) );
+		}
+
+		$file = array(
+			'name'     => basename( $url ),
+			'type'     => 'image/jpge',
+			'tmp_name' => $temp_file,
+			'error'    => 0,
+			'size'     => filesize( $temp_file ),
+		);
+
+		$overrides = array(
+			'test_form' => false,
+		);
+
+		$results = wp_handle_sideload( $file, $overrides );
+
+		if ( ! empty( $results['error'] ) ) {
+			wp_send_json_success( $results['error'] );
+		}
+
+		update_post_meta( $id_attachment, 'ear2words_status', 'done' );
+
+		wp_send_json_success( array( 'message' => 'file ricevuto' ) );
 	}
 }
 
