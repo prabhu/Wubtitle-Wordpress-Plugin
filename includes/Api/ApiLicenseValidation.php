fdc5b97dba96f983e04869c8e186f9561f9827d7
Alessio Catania <catania.alessio96@gmail.com>
2020-04-03 17:13:38 +0200

    fix

--- includes/Api/ApiLicenseValidation.php
+++ includes/Api/ApiLicenseValidation.php
@@ -65,11 +65,10 @@ class ApiLicenseValidation {
 	 * Ottiene gli uuid dei post.
 	 */
 	public function get_job_list() {
-		$args = array(
+		$args     = array(
 			'post_type'      => 'attachment',
 			'posts_per_page' => -1,
 			'meta_key'       => 'ear2words_job_uuid',
-			// TODO: Filtrare per job pending dopo allineamento al lavoro di Alessio.
 		);
 		$media    = get_posts( $args );
 		$job_list = array();
@@ -84,7 +83,3 @@ class ApiLicenseValidation {
 		return $data;
 	}
 }
-
-
-
-
