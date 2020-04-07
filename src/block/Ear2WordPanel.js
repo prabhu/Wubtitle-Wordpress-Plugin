0ad1e319e7003af2f27dd8dfc3a182e208255b54
Nicola <nicola.palermo@ctmobi.it>
2020-04-07 16:33:37 +0200

    Cambiato attiva to generate

--- src/block/Ear2WordPanel.js
+++ src/block/Ear2WordPanel.js
@@ -85,7 +85,7 @@ const Ear2WordPanel = props => {
 					isPrimary
 					onClick={onClick}
 				>
-					{__("TURN ON SUBTITLES", "ear2words")}
+					{__("GENERATE SUBTITLES", "ear2words")}
 				</Button>
 			</PanelBody>
 		</InspectorControls>
