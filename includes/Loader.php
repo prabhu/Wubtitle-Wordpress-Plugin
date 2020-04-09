acdf01e7d2c45090154718a050027a66d5ef408e
Alessio Catania <catania.alessio96@gmail.com>
2020-04-09 15:26:34 +0200

    aggiunta traduzione e fix

--- includes/Loader.php
+++ includes/Loader.php
@@ -16,7 +16,7 @@ class Loader {
 	 * Istanzia le classi Principali
 	 */
 	public static function init() {
-		// Inserire qui le classi da istanziare.
+		load_plugin_textdomain( 'ear2words', false, 'wp-ear2words/languages/' );
 		$classes = array(
 			'gutenber'             => Gutenberg\VideoBlock::class,
 			'settings'             => Dashboard\Settings::class,
