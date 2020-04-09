5a5a980bd1c8d990128b9f222ea49a0e677bff18
Nicola <nicola.palermo@ctmobi.it>
2020-04-09 16:57:42 +0200

    setup

--- includes/Core/EditVideo.php
+++ includes/Core/EditVideo.php
@@ -14,28 +14,34 @@ namespace Ear2Words\Core;
  */
 class EditVideo {
 	/**
-	 * Init class actions
+	 * Init class actions.
 	 */
 	public function run() {
-        add_filter('attachment_fields_to_edit', array( $this, 'add_subtitle_field'), 10, 2);		
-    }
-    
+		add_filter( 'attachment_fields_to_edit', array( $this, 'add_subtitle_field' ), 10, 2 );
+	}
+
+	/**
+	 * Aggiunge test area.
+	 *
+	 * @param array $form_fields textarea.
+	 * @param array $post post da modificare.
+	 */
 	public function add_subtitle_field( $form_fields, $post ) {
-        $id_releted_vtt = get_post_meta($post->ID, 'ear2words_subtitle', true);
+		$id_releted_vtt = get_post_meta( $post->ID, 'ear2words_subtitle', true );
 
-        $file_vtt = wp_get_attachment_url( $id_releted_vtt );
+		$file_vtt = wp_get_attachment_url( $id_releted_vtt );
 
-        $content = file_get_contents($file_vtt);
+		$content = wp_remote_get( $file_vtt );
 
-        $form_fields['text_field'] = array(
-            'class' => 'subtitle-textarea',
-            'label' => 'Subtitles',
-            'input' => 'textarea',
-            'value' => $content,
-        );
-        return $form_fields;
-    }
+		$form_fields['text_field'] = array(
+			'class' => 'subtitle-textarea',
+			'label' => 'Subtitles',
+			'input' => 'textarea',
+			'value' => $content,
+		);
+		return $form_fields;
+	}
 
 }
 
-?>
+
