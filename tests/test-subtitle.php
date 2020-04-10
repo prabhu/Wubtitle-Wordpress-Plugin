44b3d730f331d47477a59abe03662d8481853252
Nicola <nicola.palermo@ctmobi.it>
2020-04-10 09:37:57 +0200

    setup test

---
+++ tests/test-subtitle.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Class Subtitle Test
+ *
+ * @package Ear2words
+ */
+
+use Ear2Words\Core\Subtitle;
+
+/**
+ * Sample test case.
+ */
+class TestSubtitle extends WP_UnitTestCase {
+
+	/**
+	 * Setup del test
+	 */
+	public function setUp() {
+		$this->instance = new Subtitle();
+	}
+
+}
