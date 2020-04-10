b8b192f9e9e26ff077b9a305e7f8a09bada7861d
Nicola <nicola.palermo@ctmobi.it>
2020-04-10 13:10:54 +0200

    test

--- tests/test-subtitle.php
+++ tests/test-subtitle.php
@@ -8,7 +8,7 @@
 use Ear2Words\Core\Subtitle;
 
 /**
- * Sample test case.
+ * Subtitle test case.
  */
 class TestSubtitle extends WP_UnitTestCase {
 
@@ -16,7 +16,32 @@ class TestSubtitle extends WP_UnitTestCase {
 	 * Setup del test
 	 */
 	public function setUp() {
-		$this->instance = new Subtitle();
+		$this->instance = new Ear2Words\Core\Subtitle();
+		
+		$this->subtitle_id     = self::factory()->post->create( [
+			'is_subtitle' => 'true'
+		] );
+		$this->video_id   = self::factory()->post->create( [
+			'id' => 4,
+			'meta_input' => [
+				'ear2words_subtitle' => $this->subtitle_id
+			]
+		] );
+
 	}
+
+
+	/**
+	 * Caso positivo di delete_subtitle.
+	 */
+	public function test_delete_attachment_and_subtitle() {
+		$this->instance->run();
+		wp_delete_attachment( $this->video_id );
+		
+		$subtitle_post = get_post($this->subtitle_id);
+
+		$this->assertFalse($subtitle_post);
+	}
+
 
 }
