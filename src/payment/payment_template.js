896027932ef9b23ca5dc826f2f3745cf5745af6a
Nicola Palermo <nicola.palermo@ctmobi.it>
2020-04-20 17:51:43 +0200

    utilizzato design pattern IIFE in payment template script, lint JS non mi ha permesso l'utilizzo delle variabili inutilizzate window e undefined

--- src/payment/payment_template.js
+++ src/payment/payment_template.js
@@ -1,34 +1,46 @@
+const paymentModule = (function(Stripe, document) {
+	let stripe = null;
+
+	const openStripeForm = sessionId => {
+		if (sessionId) {
+			stripe.redirectToCheckout({ sessionId });
+		}
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		const select = document.querySelector("#select").value;
+		// TODO: cambiare con variabili globali
+		const nonce =
+			"<?php echo esc_js( wp_create_nonce( 'itr_ajax_nonce' ) ); ?>";
+
+		// TODO: cambiare con variabile globale
+		fetch("<?php echo esc_html( admin_url( 'admin-ajax.php' ) ); ?>", {
+			method: "POST",
+			credentials: "include",
+			headers: new Headers({
+				"Content-Type": "application/x-www-form-urlencoded"
+			}),
+			body: `action=submit_plan&_ajax_nonce=${nonce}&pricing_plan=${select}`
+		})
+			.then(resp => resp.json())
+			.then(data => {
+				if (data) {
+					openStripeForm(data, stripe);
+				}
+			});
+	};
+
+	const init = () => {
+		stripe = Stripe("pk_test_nfUYjFiwdkzYpPOfCZkVZiMK00lOAFcAK7");
+		const form = document.querySelector("#form");
+
+		form.addEventListener("submit", handleSubmit);
+	};
+
+	return init;
+})(Stripe, document);
+
 document.addEventListener("DOMContentLoaded", function() {
-	const stripe = Stripe("pk_test_nfUYjFiwdkzYpPOfCZkVZiMK00lOAFcAK7");
-	const form = document.querySelector("#form");
-
-	form.addEventListener("submit", e => handleSubmit(e, stripe));
+	paymentModule.init();
 });
-
-const handleSubmit = (e, stripe) => {
-	e.preventDefault();
-	const select = document.querySelector("#select").value;
-	const nonce =
-		"<?php echo esc_js( wp_create_nonce( 'itr_ajax_nonce' ) ); ?>";
-
-	fetch("<?php echo esc_html( admin_url( 'admin-ajax.php' ) ); ?>", {
-		method: "POST",
-		credentials: "include",
-		headers: new Headers({
-			"Content-Type": "application/x-www-form-urlencoded"
-		}),
-		body: `action=submit_plan&_ajax_nonce=${nonce}&pricing_plan=${select}`
-	})
-		.then(resp => resp.json())
-		.then(function(data) {
-			if (data) {
-				openStripeForm(data, stripe);
-			}
-		});
-};
-
-const openStripeForm = (sessionId, stripe) => {
-	if (sessionId) {
-		stripe.redirectToCheckout({ sessionId });
-	}
-};
