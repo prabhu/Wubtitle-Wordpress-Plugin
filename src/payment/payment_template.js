aa637e94cc150b927cf431595ba089014b24f3e7
Nicola Palermo <nicola.palermo@ctmobi.it>
2020-04-20 08:59:40 +0200

    usato fetch

--- src/payment/payment_template.js
+++ src/payment/payment_template.js
@@ -11,18 +11,24 @@ const handleSubmit = (e, stripe) => {
 	const nonce =
 		"<?php echo esc_html( wp_create_nonce( 'itr_ajax_nonce' ) ); ?>";
 
-	const request = new XMLHttpRequest();
-	request.open(
-		"POST",
-		"<?php echo esc_html( admin_url( 'admin-ajax.php' ) ); ?>",
-		true
-	);
-	request.onload = function() {
-		if (this.status >= 200 && this.status < 400) {
-			const response = JSON.parse(this.response);
-			if (response.success) {
+	const data = {
+		action: "submit_plan",
+		_ajax_nonce: nonce,
+		pricing_plan: select
+	};
+
+	fetch("<?php echo esc_html( admin_url( 'admin-ajax.php' ) ); ?>", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify(data)
+	})
+		.then(response => response.json())
+		.then(res => {
+			if (res) {
 				stripe.redirectToCheckout({
-					sessionId: response.data
+					sessionId: res
 				});
 				// .then(function(result) {
 				// 	const messageError = result.error.message;
@@ -30,13 +36,9 @@ const handleSubmit = (e, stripe) => {
 
 				// });
 			}
-		}
-	};
-	request.setRequestHeader(
-		"Content-type",
-		"application/x-www-form-urlencoded"
-	);
-	request.send(
-		"action=submit_plan&_ajax_nonce=" + nonce + "&pricing_plan=" + select
-	);
+		});
+	//  TODO: Gestire errore. Rimosso per lintJS.
+	// .catch(error => {
+	// 	// console.error("Error:", error);
+	// });
 };
