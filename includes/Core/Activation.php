8a799697679d448a0d3688fbff8e5de3dbf6c681
Alessio Catania <alessio.catania@ctmobi.it>
2020-04-27 12:12:44 +0200

    all'attivazione setto un flag e ricevo la licenza

---
+++ includes/Core/Activation.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Questo file implementa le funzioni che vengono eseguite all'attivazione del plugin.
+ *
+ * @author     Alessio Catania
+ * @since      0.1.0
+ * @package    Ear2Words\Api
+ */
+
+namespace Ear2Words\Core;
+
+/**
+ * Questa classe mplementa le funzioni che vengono eseguite all'attivazione del plugin.
+ */
+class Activation {
+	/**
+	 * Init class action.
+	 */
+	public function run() {
+		register_activation_hook( EAR2WORDS_FILE_URL, array( $this, 'ear2words_activation_license_key' ) );
+	}
+
+	/**
+	 * All'attivazione del plugin chiama l'endpoint per ricevere la license key.
+	 */
+	public function ear2words_activation_license_key() {
+		$site_url      = get_site_url();
+		$body          = array(
+			'data' => array(
+				'domainUrl' => $site_url,
+			),
+		);
+		$response      = wp_remote_post(
+			ENDPOINT . 'key/create',
+			array(
+				'method'  => 'POST',
+				'headers' => array(
+					'Content-Type' => 'application/json; charset=utf-8',
+				),
+				'body'    => wp_json_encode( $body ),
+			)
+		);
+		$code_response = wp_remote_retrieve_response_code( $response );
+		if ( 201 === $code_response && empty( get_options( 'ear2words_license_key' ) ) ) {
+			$response_body = json_decode( wp_remote_retrieve_body( $response ) );
+			update_option( 'ear2words_free', true );
+			update_option( 'ear2words_license_key', $response_body->data->licenseKey );
+		}
+	}
+}
--- includes/Loader.php
+++ includes/Loader.php
@@ -33,6 +33,7 @@ class Loader {
 			'extented_media_library' => MediaLibrary\MediaLibraryExtented::class,
 			'send_pricing_plan'      => Api\ApiPricingPlan::class,
 			'payment_template'       => Dashboard\PaymentTemplate::class,
+			'activation'             => Core\Activation::class,
 		);
 
 		foreach ( $classes as $key => $class ) {
