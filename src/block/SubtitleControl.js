1dc0d2925a4c809eb5d5a5c8ec3355d0738b6941
Nicola Palermo <nicola.palermo@ctmobi.it>
2020-04-30 12:51:54 +0200

    aggiunto margine ai controlli gutenberg

--- src/block/SubtitleControl.js
+++ src/block/SubtitleControl.js
@@ -31,8 +31,12 @@ const SubtitleControl = ({ statusText, langText, isPublished, postId }) => {
 
 	return (
 		<Fragment>
-			<div>{__("Status: ", "ear2words") + statusExten[statusText]}</div>
-			<div>{__("Language: ", "ear2words") + langExten[langText]}</div>
+			<div style={{ marginBottom: "8px" }}>
+				{__("Status: ", "ear2words") + statusExten[statusText]}
+			</div>
+			<div style={{ margin: "16px 0" }}>
+				{__("Language: ", "ear2words") + langExten[langText]}
+			</div>
 			<ToggleControl
 				label="Published"
 				checked={isPublished}
