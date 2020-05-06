c3d16ff8abb72e176fde8c7fff9bb493743d6a71
Nicola Palermo <nicola.palermo@ctmobi.it>
2020-05-06 12:52:44 +0200

    aggiunta classe nel loader

--- includes/Loader.php
+++ includes/Loader.php
@@ -38,6 +38,7 @@ class Loader {
 			'cancel_subscription'     => Api\ApiCancelSubscription::class,
 			'cron'                    => Core\Cron::class,
 			'register_callback_pages' => Dashboard\RegisterStripeCallbackPages::class,
+			'api_auth_plan'           => Api\ApiAuthUpgradePlan::class,
 		);
 
 		foreach ( $classes as $key => $class ) {
