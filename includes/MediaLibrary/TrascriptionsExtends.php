a0d3e4af408586a9364ea0b7ff3dda79795a9621
Alessio Catania <alessio.catania@ctmobi.it>
2020-05-15 16:53:43 +0200

    corretto il comportamento della modale

--- includes/MediaLibrary/TrascriptionsExtends.php
+++ includes/MediaLibrary/TrascriptionsExtends.php
@@ -20,17 +20,26 @@ class TrascriptionsExtends {
 	 */
 	public function run() {
 		add_filter( 'embed_oembed_html', array( $this, 'ear2words_embed_shortcode' ), 10, 4 );
-		add_action( 'media_buttons', array( $this, 'add_my_media_button' ), 15 );
-		add_action( 'wp_enqueue_media', array( $this, 'include_media_button_js_file' ) );
-		add_filter( 'media_upload_tabs', array( $this, 'my_upload_tab' ) );
-		add_action( 'media_upload_videotranscriptions', array( $this, 'add_my_new_form' ) );
+		add_action( 'media_buttons', array( $this, 'add_transcriptions_media_button' ), 15 );
+		add_action( 'wp_enqueue_media', array( $this, 'include_transcription_modal_script' ) );
 	}
 	/**
 	 * Fa l'override dello shortcode.
 	 *
 	 * @param string $html html dello shortcode nativo.
+	 * @param string $url url del video.
+	 * @param array  $attr attributi dello shortcode.
 	 */
-	public function ear2words_embed_shortcode( $html ) {
+	public function ear2words_embed_shortcode( $html, $url, $attr ) {
+		$url_parts    = wp_parse_url( $url );
+		$allowed_urls = array(
+			'www.youtube.com',
+			'www.youtu.be',
+		);
+		if ( 'enable' !== $attr['transcription'] || ! in_array( $url_parts['host'], $allowed_urls, true ) ) {
+			return $html;
+		}
+		// TODO eseguire la funzione per creare un nuovo custom posttype e inserire lo shortcode.
 		$html .= '<p> prova transcription </p>';
 		return $html;
 	}
@@ -41,49 +50,15 @@ class TrascriptionsExtends {
 		wp_iframe( array( $this, 'my_new_form' ) );
 	}
 	/**
-	 * Render del frame.
-	 */
-	public function my_new_form() {
-		// phpcs:disable
-		echo media_upload_header(); // This function is used for print media uploader headers etc.
-		ob_start();
-		?>
-				<div style="padding: 16px; display: block;">
-					<input style="padding: 12px 40px 12px 14px; width: 100%;" id="embed-url-field" type="url" placeholder="Inserisci dall'URL">
-					<button id="wub-add-transcriptions" class="button-primary"> Add Transcriptions </button>
-				</div>
-				<script type="text/javascript">
-					const addTranscription = document.getElementById("wub-add-transcriptions");
-					if (addTranscription) {
-						addTranscription.addEventListener("click", () => {
-							window.close();
-						});
-					}
-				</script>
-		<?php
-		echo ob_get_clean();
-		// phpcs:enable
-	}
-	/**
-	 * Aggiunge un tab alla modale del media upload.
-	 */
-	public function my_upload_tab() {
-		$tabs                        = array();
-		$tabs['videotranscriptions'] = __( 'Transcriptions', 'ear2words' );
-		return $tabs;
-	}
-	/**
 	 * Include il file javascript.
 	 */
-	public function include_media_button_js_file() {
-		//phpcs:disable
-		wp_enqueue_script( 'media_button', EAR2WORDS_URL . '/src/editor/media_button.js' );
-		//phpcs:enable
+	public function include_transcription_modal_script() {
+		wp_enqueue_script( 'transcription_modal_script', EAR2WORDS_URL . '/src/editor/transcriptionModalScript.js', null, 'transcription_script', true );
 	}
 	/**
 	 * Aggiunge il bottone custom.
 	 */
-	public function add_my_media_button() {
+	public function add_transcriptions_media_button() {
 		echo '<a href="#" id="insert-my-media" class="button">Add transcription</a>';
 	}
 }
