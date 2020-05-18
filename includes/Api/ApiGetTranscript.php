96fbdd558365c6e9fdccfdf691f1dd3f388c57a3
Nicola Palermo <nicola.palermo@ctmobi.it>
2020-05-18 21:19:47 +0200

    rimosse modifiche

--- includes/Api/ApiGetTranscript.php
+++ includes/Api/ApiGetTranscript.php
@@ -46,42 +46,12 @@ class ApiGetTranscript {
 				default:
 					return;
 			}
-
-			$response = $video_source->send_job_to_backend( $id_video );
-
-			$response_code = wp_remote_retrieve_response_code( $response );
-
-			if ( 201 !== $response_code ) {
-				wp_send_json_success( $this->handle_backend_error( $response_code ) );
-				wp_die();
-			}
-
 			$transcript = $video_source->get_subtitle( $id_video, $from );
 
 			wp_send_json_success( $transcript );
 			wp_die();
 		}
 		wp_die();
-	}
-
-	/**
-	 * Gestisce il messaggio d'errore.
-	 *
-	 * @param int $response_code response code della chiamata al backend.
-	 */
-	public function handle_backend_error( $response_code ) {
-		switch ( $response_code ) {
-			case 400:
-				return __( 'Some issues with the request. Try again in a few minutes or contact the support.', 'ear2words' );
-			case 401:
-				return __( 'Unauthorized. Check your license key.', 'ear2words' );
-			case 403:
-				return __( 'Forbidden. Check your license key.', 'ear2words' );
-			case 429:
-				return __( 'Too Many Requests. Try again in a few minutes.', 'ear2words' );
-			default:
-				return __( 'Error.', 'ear2words' );
-		}
 	}
 
 }
