cd848813a300dafbb6a776054dc7d150f7d35055
Nicola Palermo <nicola.palermo@ctmobi.it>
2020-05-22 14:24:17 +0200

    test modale

--- src/block/YoutubeControlPanel.js
+++ src/block/YoutubeControlPanel.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 import { useSelect } from "@wordpress/data";
-import { PanelBody, Button } from "@wordpress/components";
+import { PanelBody, Button, SelectControl } from "@wordpress/components";
 import { InspectorControls } from "@wordpress/block-editor";
 import { __ } from "@wordpress/i18n";
 import { useState } from "@wordpress/element";
@@ -28,7 +28,7 @@ const YoutubeControlPanel = props => {
 		}
 	});
 
-	const onClick = () => {
+	const handleClick = () => {
 		setMessage(__("Getting transcript...", "ear2words"));
 		wp.ajax
 			.send("get_transcript", {
@@ -52,17 +52,61 @@ const YoutubeControlPanel = props => {
 			});
 	};
 
+	const getLang = () => {
+		wp.ajax
+			.send("get_video_info", {
+				type: "POST",
+				data: {
+					url: props.url
+				}
+			})
+			.then(response => {
+				setReady(true);
+				response.languages.forEach(lang => {
+					selectOptions[lang.baseUrl] = lang.name.simpleText;
+				});
+				console.log(selectOptions);
+			})
+			.fail(response => {
+				console.log(response);
+			});
+	};
+
+	const [languageSelected, setLanguage] = useState("");
+	const [langReady, setReady] = useState(false);
+
+	const selectOptions = [
+		{
+			value: "none",
+			label: __("Select lang", "ear2words")
+		}
+	];
+
 	return (
 		<InspectorControls>
 			<PanelBody title="Wubtitle">
 				<p style={{ margin: "0" }}>
 					{`${__("Status: ", "ear2words")} ${status}`}
 				</p>
+
+				{langReady ? (
+					<SelectControl
+						label={__("Select the video language", "ear2words")}
+						value={languageSelected}
+						onChange={lingua => {
+							setLanguage(lingua);
+						}}
+						options={selectOptions}
+					/>
+				) : (
+					getLang()
+				)}
+				{/* <ModalLang /> */}
 				<Button
 					name="sottotitoli"
 					id={props.id}
 					isPrimary
-					onClick={onClick}
+					onClick={handleClick}
 					disabled={isDisabled}
 				>
 					{__("Get Transcribe", "ear2words")}
