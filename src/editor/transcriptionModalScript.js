7021aa5173a86cfd7af43d36ba95df78482c1e22
Alessio Catania <alessio.catania@ctmobi.it>
2020-05-22 15:11:50 +0200

    risolto punto di review

--- src/editor/transcriptionModalScript.js
+++ src/editor/transcriptionModalScript.js
@@ -2,8 +2,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 	const pattern = new RegExp(
 		"^(https?:\\/\\/)?" + // protocol
-		"((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
-		"((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+		"((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,})" + // domain name
 		"(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
 		"(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
 			"(\\#[-a-z\\d_]*)?", // fragment locator
