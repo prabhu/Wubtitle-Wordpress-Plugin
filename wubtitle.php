adebf65f5623fde397acf5c92b84efc40171e913
Alessio Catania <alessio.catania@ctmobi.it>
2020-06-30 17:35:22 +0200

    fix

--- wubtitle.php
+++ wubtitle.php
@@ -22,7 +22,6 @@ define( 'WUBTITLE_DIR', plugin_dir_path( __FILE__ ) );
 define( 'WUBTITLE_URL', plugin_dir_url( __FILE__ ) );
 define( 'WUBTITLE_NAME', dirname( plugin_basename( __FILE__ ) ) );
 define( 'WUBTITLE_VER', '0.1.1' );
-define( 'WUBTITLE_ENDPOINT', 'https://api.wubtitle.com/' );
 $endpoint = 'https://api.wubtitle.com/';
 if ( defined( 'WP_WUBTITLE_ENV' ) && 'development' === WP_WUBTITLE_ENV ) {
 	$endpoint = 'https://dev.api.wubtitle.com/';
