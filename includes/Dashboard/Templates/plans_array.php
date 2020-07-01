120c4667411ac175c65828a174d5612f0d612042
Alessio Catania <alessio.catania@ctmobi.it>
2020-07-01 17:10:25 +0200

    get info plans from options

--- includes/Dashboard/Templates/plans_array.php
+++ includes/Dashboard/Templates/plans_array.php
@@ -13,94 +13,22 @@
 
 wp_cache_delete( 'wubtitle_plan', 'options' );
 wp_cache_delete( 'wubtitle_is_first_month', 'options' );
-$plans = array(
-	array(
-		'stripe_code'    => 'plan_0',
-		'name'           => __( 'Free', 'wubtitle' ),
-		'price'          => 0,
-		'features'       => array(
-			__( 'Number of video', 'wubtitle' )        => __( '3', 'wubtitle' ),
-			__( 'Total length of videos', 'wubtitle' ) => __( '30 min', 'wubtitle' ),
-		),
-		'dot_list'       => array(
-			__( 'Mp4 Video format allowed', 'wubtitle' ),
-			__( 'Recognized languages: English and Italian', 'wubtitle' ),
-		),
-		'zoom'           => false,
-		'current_plan'   => true,
-		'icon'           => 'smile.svg',
-		'class_button'   => 'button-choose-plan',
-		'message_button' => __( 'Choose this plan', 'wubtitle' ),
-	),
-	array(
-		'stripe_code'    => 'plan_HBBbNjLjVk3w4w',
-		'name'           => __( 'Professional', 'wubtitle' ),
-		'price'          => 19,
-		'features'       => array(
-			__( 'Number of video', 'wubtitle' )        => __( '10', 'wubtitle' ),
-			__( 'Total length of videos', 'wubtitle' ) => __( '3 hours', 'wubtitle' ),
-		),
-		'dot_list'       => array(
-			__( 'All wordpress formats supported by wordpress', 'wubtitle' ),
-			__( 'Recognized languuages: English, Italian, German, French, Spanish and Chinese', 'wubtitle' ),
-		),
-		'zoom'           => false,
-		'current_plan'   => false,
-		'icon'           => 'fire.svg',
-		'class_button'   => 'button-choose-plan',
-		'message_button' => __( 'Choose this plan', 'wubtitle' ),
-	),
-	array(
-		'stripe_code'    => 'plan_HBBS5I9usXvwQR',
-		'name'           => __( 'Elite', 'wubtitle' ),
-		'price'          => 49,
-		'features'       => array(
-			__( 'Number of video', 'wubtitle' )        => __( '30', 'wubtitle' ),
-			__( 'Total length of videos', 'wubtitle' ) => __( '10 hours', 'wubtitle' ),
-		),
-		'dot_list'       => array(
-			__( 'All wordpress formats supported by wordpress', 'wubtitle' ),
-			__( 'Recognized languuages: English, Italian, German, French, Spanish and Chinese', 'wubtitle' ),
-		),
-		'zoom'           => false,
-		'current_plan'   => false,
-		'icon'           => 'rocket.svg',
-		'class_button'   => 'button-choose-plan',
-		'message_button' => __( 'Choose this plan', 'wubtitle' ),
-	),
-);
-
+$plans                     = get_option( 'wubtitle_plans' );
+$currentplan               = get_option( 'wubtitle_plan' );
+$is_first_month            = get_option( 'wubtitle_is_first_month' );
 $disable_downgrade_message = __( 'Unable this select this plan during the first month of subscription for current plan', 'wubtitle' );
-switch ( get_option( 'wubtitle_plan' ) ) {
-	case 'plan_0':
-		$plans[0]['class_button']   = 'current-plan';
-		$plans[0]['message_button'] = __( 'Your plan', 'wubtitle' );
-		break;
-	case 'plan_HBBbNjLjVk3w4w':
-		if ( get_option( 'wubtitle_is_first_month' ) ) {
-			$plans[0]['class_button']   = 'disable-downgrade';
-			$plans[0]['message_button'] = $disable_downgrade_message;
-		}
-		$plans[1]['class_button']   = 'current-plan';
-		$plans[1]['message_button'] = __( 'Your plan', 'wubtitle' );
-		break;
-	case 'plan_HBBS5I9usXvwQR':
-		if ( get_option( 'wubtitle_is_first_month' ) ) {
-			$plans[0]['class_button']   = 'disable-downgrade';
-			$plans[1]['class_button']   = 'disable-downgrade';
-			$plans[0]['message_button'] = $disable_downgrade_message;
-			$plans[1]['message_button'] = $disable_downgrade_message;
-		}
-		$plans[2]['class_button']   = 'current-plan';
-		$plans[2]['message_button'] = __( 'Your plan', 'wubtitle' );
-		break;
-}
-switch ( get_option( 'wubtitle_plan' ) ) {
-	case 'plan_0':
-		$plans[1]['zoom'] = true;
-		break;
-	case 'plan_HBBbNjLjVk3w4w':
-	case 'plan_HBBS5I9usXvwQR':
-		$plans[2]['zoom'] = true;
-		break;
+
+foreach ( $plans as $key => $plan ) {
+	$plans[ $key ]['current_plan']   = false;
+	$plans[ $key ]['class_button']   = 'button-choose-plan';
+	$plans[ $key ]['message_button'] = __( 'Choose this plan', 'wubtitle' );
+	$plans[ $key ]['features']       = array(
+		__( 'Number of video', 'wubtitle' )        => $plans[ $key ]['totalJobs'],
+		__( 'Total length of videos', 'wubtitle' ) => date_i18n( 'H:i', $plans[ $key ]['totalSeconds'] ) . ' h',
+	);
+	if ( $plans[ $key ]['stripe_code'] === $currentplan ) {
+		$plans[ $key ]['current_plan']   = true;
+		$plans[ $key ]['class_button']   = 'current-plan';
+		$plans[ $key ]['message_button'] = __( 'Your plan', 'wubtitle' );
+	}
 }
