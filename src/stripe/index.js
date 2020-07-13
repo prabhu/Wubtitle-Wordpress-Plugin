86f83d5cafcbe6f909edaa5925410d00ada70071
Alessio Catania <alessio.catania@ctmobi.it>
2020-07-13 18:50:58 +0200

    modify loader logic

--- src/stripe/index.js
+++ src/stripe/index.js
@@ -1,19 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CheckoutForm from './Components/CheckoutForm';
-import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
+/**
+ * Template in react for custom stripe form
+ */
 
-// Make sure to call `loadStripe` outside of a component’s render to avoid
-// recreating the `Stripe` object on every render.
-const stripePromise = loadStripe('pk_test_nfUYjFiwdkzYpPOfCZkVZiMK00lOAFcAK7');
-
-function App() {
-	return (
-		<Elements stripe={stripePromise}>
-			<CheckoutForm />
-		</Elements>
-	);
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import './SubscriptionForm';
