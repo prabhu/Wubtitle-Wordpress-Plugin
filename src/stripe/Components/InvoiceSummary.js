22a343c6ff2775e3d2b789c8cfc26653465e9d85
Nicola Palermo <nicola.palermo@ctmobi.it>
2020-07-20 18:20:48 +0200

    billing summary

--- src/stripe/Components/InvoiceSummary.js
+++ src/stripe/Components/InvoiceSummary.js
@@ -1,38 +1,61 @@
 import { __ } from '@wordpress/i18n';
 export default function InvoiceSummary(props) {
-	const { invoiceValues, price } = props;
+	const { invoiceValues } = props;
 
 	return (
-		<div>
-			<h2>{__('Price', 'wubtitle')}</h2>
+		<div className="summary">
+			<h2>{__('Billing Recap', 'wubtitle')}</h2>
 			<p>
-				<strong>{price}</strong>+<strong>{invoiceValues.tax}</strong>
+				<strong>{__('Name', 'wubtitle')}: </strong>
+				{invoiceValues.invoice_name}
 			</p>
-			<h2>{__('Summary', 'wubtitle')}</h2>
-			<h3>{__('Name', 'wubtitle')}</h3>
-			<p>{invoiceValues.invoice_name}</p>
-			<h3>{__('lastname', 'wubtitle')}</h3>
-			<p>{invoiceValues.invoice_lastname}</p>
-			<h3>Email</h3>
-			<p>{invoiceValues.invoice_email}</p>
-			<h3>{__('Address', 'wubtitle')}</h3>
-			<p>{invoiceValues.address}</p>
-			<h3>{__('Postal Code', 'wubtitle')}</h3>
-			<p>{invoiceValues.cap}</p>
-			<h3>{__('City', 'wubtitle')}</h3>
-			<p>{invoiceValues.city}</p>
-			<h3>{__('Company Name', 'wubtitle')}</h3>
-			<p>{invoiceValues.company_name}</p>
-			<h3>{__('Country', 'wubtitle')}</h3>
-			<p>{invoiceValues.country}</p>
-			<h3>{__('Fiscal Code', 'wubtitle')}</h3>
-			<p>{invoiceValues.fiscal_code}</p>
-			<h3>{__('Province', 'wubtitle')}</h3>
-			<p>{invoiceValues.province}</p>
-			<h3>{__('Telephone', 'wubtitle')}</h3>
-			<p>{invoiceValues.telephone}</p>
-			<h3>{__('Vat Code', 'wubtitle')}</h3>
-			<p>{invoiceValues.vat_code}</p>
+			<p>
+				<strong>{__('Email', 'wubtitle')}: </strong>
+				{invoiceValues.invoice_email}
+			</p>
+			<p>
+				<strong>{__('Country', 'wubtitle')}: </strong>
+				{invoiceValues.country}
+			</p>
+			<p>
+				<strong>{__('Postal Code', 'wubtitle')}: </strong>
+				{invoiceValues.cap}
+			</p>
+			<p>
+				<strong>{__('Address', 'wubtitle')}: </strong>
+				{invoiceValues.address}
+			</p>
+			<p>
+				<strong>{__('VAT Code', 'wubtitle')}: </strong>
+				{invoiceValues.vat_code}
+			</p>
+
+			<p>-----------------------</p>
+
+			<p>
+				<strong>{__('Lastname', 'wubtitle')}: </strong>
+				{invoiceValues.invoice_lastname}
+			</p>
+			<p>
+				<strong>{__('Company Name', 'wubtitle')}: </strong>
+				{invoiceValues.company_name}
+			</p>
+			<p>
+				<strong>{__('Province', 'wubtitle')}: </strong>
+				{invoiceValues.province}
+			</p>
+			<p>
+				<strong>{__('City', 'wubtitle')}: </strong>
+				{invoiceValues.city}
+			</p>
+			<p>
+				<strong>{__('Fiscal Code', 'wubtitle')}: </strong>
+				{invoiceValues.fiscal_code}
+			</p>
+			<p>
+				<strong>{__('Destination Code', 'wubtitle')}: </strong>
+				{invoiceValues.destination_code}
+			</p>
 		</div>
 	);
 }
