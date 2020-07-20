c88a044a7f9466c377708edfddf37fc44b3aa6d4
Nicola Palermo <nicola.palermo@ctmobi.it>
2020-07-20 15:09:43 +0200

    form inputs columns

--- src/stripe/Components/InfoPriceColumn.js
+++ src/stripe/Components/InfoPriceColumn.js
@@ -8,12 +8,36 @@ const InfoPriceColumn = () => {
 	return (
 		<div className="column price-column">
 			<div className="price">
-				<p>Price</p>
-				<p>a</p>
+				<p>Subscribe to professional plan</p>
+				<p>
+					Price<span>19</span>
+				</p>
+				<p>
+					Vat<span>111</span>
+				</p>
+				<p>
+					Total<span>199</span>per month
+				</p>
 			</div>
 			<div className="disclaimer">
 				<p>lorem lorevareg sdgv ergvre grgergerge gergreg wefrgwergw</p>
-				<p>terms and cond</p>
+				<p>
+					<a
+						href="https://google.it"
+						rel="noreferrer"
+						target="_blank"
+					>
+						terms and cond
+					</a>
+					|
+					<a
+						href="https://google.it"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Privacy
+					</a>
+				</p>
 			</div>
 		</div>
 	);
