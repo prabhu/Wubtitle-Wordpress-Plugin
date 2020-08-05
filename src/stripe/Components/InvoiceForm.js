1087dd93b931ce36ac24adcf349c0aba4a242424
Alessio Catania <alessio.catania@ctmobi.it>
2020-08-05 19:07:31 +0200

    dynamic load modules

--- src/stripe/Components/InvoiceForm.js
+++ src/stripe/Components/InvoiceForm.js
@@ -1,11 +1,11 @@
+import { useEffect } from 'react';
 import * as Yup from 'yup';
 import { __ } from '@wordpress/i18n';
 import { Formik, Form, Field } from 'formik';
 import countries from '../data/countries.json';
 import provinces from '../data/provinces.json';
 import euCountries from '../data/europeanCountries.json';
-import CodiceFiscale from 'codice-fiscale-js';
-
+let FiscalCodeModule;
 export default function CheckoutForm(props) {
 	const {
 		invoicePreValues,
@@ -14,8 +14,13 @@ export default function CheckoutForm(props) {
 		error,
 		loading,
 	} = props;
+	useEffect(() => {
+		async function LoadModule() {
+			FiscalCodeModule = await import('codice-fiscale-js').default;
+		}
+		LoadModule();
+	}, []);
 	const requiredMessage = __('Required', 'wubtitle');
-
 	const DisplayingErrorMessagesSchema = Yup.lazy((values) => {
 		const yupObject = {
 			invoice_name: Yup.string().required(requiredMessage),
@@ -53,9 +58,11 @@ export default function CheckoutForm(props) {
 			}
 		} else if (values.country === 'IT') {
 			yupObject.fiscal_code = Yup.string().test(
-				'',
+				'is-valid-fiscal-code',
 				__('Invalid fiscal code', 'wubtitle'),
-				(value) => CodiceFiscale.check(value)
+				(value) => {
+					return FiscalCodeModule.check(value);
+				}
 			);
 		}
 		if (values.country === 'IT') {
