438d21fafa54d998bd49471f49c117050251c5df
Alessio Catania <alessio.catania@ctmobi.it>
2020-08-05 18:27:36 +0200

    add notice coupon applied

--- src/stripe/Components/PriceTable.js
+++ src/stripe/Components/PriceTable.js
@@ -15,46 +15,55 @@ const PriceTable = (props) => {
 		taxAmount = discountedPrice.newTax;
 	}
 	return (
-		<table className="price-table">
-			<tr>
-				<td>{__('Price', 'wubtitle')}</td>
-				{discountedPrice ? (
-					<td className="val">
-						<span className="cut-vat">
-							{price} &euro;
-							<span className="cut-line" />
-						</span>
-						{parseFloat(discountedPrice.price)} &euro;
+		<div>
+			<table className="price-table">
+				<tr>
+					<td>{__('Price', 'wubtitle')}</td>
+					{discountedPrice ? (
+						<td className="val">
+							<span className="cut-vat">
+								{price} &euro;
+								<span className="cut-line" />
+							</span>
+							{parseFloat(discountedPrice.price)} &euro;
+						</td>
+					) : (
+						<td className="val">{price} &euro;</td>
+					)}
+				</tr>
+				<tr>
+					<td>
+						{__('VAT', 'wubtitle')} ({taxPercentage}%)
 					</td>
-				) : (
-					<td className="val">{price} &euro;</td>
-				)}
-			</tr>
-			<tr>
-				<td>
-					{__('VAT', 'wubtitle')} ({taxPercentage}%)
-				</td>
-				{taxable ? (
-					<td className="val">{taxAmount} &euro;</td>
-				) : (
+					{taxable ? (
+						<td className="val">{taxAmount} &euro;</td>
+					) : (
+						<td className="val">
+							0 &euro;{' '}
+							<span className="description">
+								{__('no Vat due for you', 'wubtitle')}
+							</span>
+						</td>
+					)}
+				</tr>
+				<tr className="total">
+					<td>{__('Total', 'wubtitle')}</td>
 					<td className="val">
-						0 &euro;{' '}
-						<span className="description">
-							{__('no Vat due for you', 'wubtitle')}
+						{total} &euro;
+						<span className="valxm">
+							{__(' per month', 'wubtitle')}
 						</span>
 					</td>
-				)}
-			</tr>
-			<tr className="total">
-				<td>{__('Total', 'wubtitle')}</td>
-				<td className="val">
-					{total} &euro;
-					<span className="valxm">
-						{__(' per month', 'wubtitle')}
-					</span>
-				</td>
-			</tr>
-		</table>
+				</tr>
+			</table>
+			{discountedPrice ? (
+				<p className="coupon-notice">
+					{__('Coupon applied!', 'wubtitle')}
+				</p>
+			) : (
+				''
+			)}
+		</div>
 	);
 };
 
